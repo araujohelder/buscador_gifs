1a620fd07fa8c07469a182afad9d4b420a989085
Helder <you@example.com>
2019-03-11 23:47:32 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:buscador_gifs/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(hintColor: Colors.white),
+    home: HomePage(),
+  ));
+}
+
